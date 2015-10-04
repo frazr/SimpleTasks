98a032abd15d23d68c29cedf342dcea864ce1819
Nicklas Ring <niklas@enoas.se>
2015-10-04 13:18:46 +0200

    del

--- 1.py
+++
@@ -1,5 +0,0 @@
-from listfunctions import minmax
-
-a, b = minmax([1, 2, 3, 44, 12])
-print a
-print b
--- listfunctions.py
+++
@@ -1,9 +0,0 @@
-# listfunctions.py
-
-def minmax(seq):
-    return min(seq), max(seq)
-
-if __name__ == "__main__":
-    s = [10, 20, 30, 40]
-    minV, maxV = minmax(s)
-    print minV, maxV
